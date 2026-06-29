c6cbf4f6ef4a082846cf44cea315ba770467f6c0
greatstackuser <user.greatstack@gmail.com>
2026-06-29 13:13:21 +0100

    Removed cors to fix Render build

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
